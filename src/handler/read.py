3bdabe99a6f16f5f3acc8d0f445d47550eab82e9
lmqwudi <283317717@qq.com>
2014-09-12 03:16:39 -0700

    ...

---
+++ src/handler/read.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+import tornado.web
+from conf.config import PREFIX
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            'read/index.html',
+            prefix=PREFIX
+        )
--- src/handler/route.py
+++ src/handler/route.py
@@ -4,6 +4,7 @@ from handler import (
     index,
     game,
     blog,
+    read,
 )
 
 
@@ -23,7 +24,11 @@ blog_handlers = [
     (r'/blog/first', blog.FirstHandler),
 ]
 
+read_handlers = [
+    (r'/read', read.IndexHandler),
+]
+
 handlers = (
     index_handlers + game_handlers
-    + blog_handlers
+    + blog_handlers + read_handlers
 )
